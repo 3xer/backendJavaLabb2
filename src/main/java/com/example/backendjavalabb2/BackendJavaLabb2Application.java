cf0dd01fecea4cbb13666076063548e517354c52
3xer <pontuslundholm1@gmail.com>
2023-11-28 15:58:59 +0100

    init setup

---
+++ src/main/java/com/example/backendjavalabb2/BackendJavaLabb2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendjavalabb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaLabb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaLabb2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendjavalabb2/BackendJavaLabb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendjavalabb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaLabb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
